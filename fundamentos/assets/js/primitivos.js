050f4356e7e7244891e63674bf4ecae5df5054ff
Pedro Perez <pedrodrums2015@gmail.com>
2022-04-20 12:59:07 -0400

    agregando archivo primitivo

---
+++ fundamentos/assets/js/primitivos.js
@@ -0,0 +1,38 @@
+// String
+let userName = "Juan Valdez";
+console.log(userName);
+
+userName = "Juanito Valdez";
+console.log(userName);
+
+userName = "Pablo";
+userName = "Fernando";
+
+console.log(typeof userName);
+
+// Boolean
+let a = false;
+console.log(typeof a);
+
+// Number
+
+let age = 45;
+console.log(typeof age);
+
+age = 33.001;
+console.log(typeof age);
+
+// Undefine
+let b;
+console.log(typeof b);
+
+// Null
+let x = null;
+console.log(typeof x);
+
+// Symbol
+let symbol1 = Symbol("a");
+let symbol2 = Symbol("a");
+console.log(typeof symbol1);
+
+console.log(symbol1 === symbol2);
