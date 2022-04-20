4f921f3a8c678cf1555112c4016b31ccc614da4a
Pedro Perez <pedrodrums2015@gmail.com>
2022-04-20 12:18:46 -0400

    iniciando proyecto

---
+++ fundamentos/assets/js/alert.js
@@ -0,0 +1,9 @@
+// alert("hola mundo");
+
+// let userName = prompt("¿Como te llamas?");
+// console.log(userName);
+// console.log("***" + userName + "***");
+
+// const selectOption = confirm("¿Estas seguro?");
+
+// console.log(selectOption);
---
+++ fundamentos/assets/js/app.js
@@ -0,0 +1,19 @@
+// Variables
+let a = 10,
+  b = 10,
+  c = "Juan",
+  d = "Pablo",
+  f = "Hola ",
+  x = a + b;
+
+const saludo = f + d;
+
+// console.log(saludo);
+
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+f = "Hola de nuevo ";
+
+// console.table({ a, b, c, d, x });
