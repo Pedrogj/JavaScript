410ec0dae0f4f215e9eccef8e28a42c4221dd7ce
Pedro Perez <pedrodrums2015@gmail.com>
2022-04-27 10:39:15 -0400

    add file: juego.js - agregando lógica del juego

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -10,12 +10,17 @@ const typesCards = ["C", "D", "H", "S"];
 const specials = ["A", "J", "Q", "K"];
 
 let playerPoints = 0,
-  machine = 0;
+  machinePoints = 0;
 
 // Referencias del HTML
 const btnAsk = document.querySelector("#btnAskForAletter");
+const btnStop = document.querySelector("#btnStop");
+const btnNewGame = document.querySelector("#btnNewGame");
+
 const divLetterPlayer = document.querySelector("#jugador-cartas");
-const smalls = document.querySelectorAll("small");
+const divLetterMachine = document.querySelector("#computadora-cartas");
+
+const smallsPoints = document.querySelectorAll("small");
 
 // Esta funcion crea un nuevo deck
 const createDeck = () => {
@@ -49,11 +54,45 @@ const takeLetter = () => {
   return letter;
 };
 
-// Pedir Carta
+// valor de la cata
 const letterValue = (letter) => {
   const value = letter.substring(0, letter.length - 1);
 
   return isNaN(value) ? (value === "A" ? 11 : 10) : parseInt(value);
+};
+
+// turno de la computadora
+const computerShift = (minPoints) => {
+  do {
+    const letter = takeLetter();
+
+    machinePoints = machinePoints + letterValue(letter);
+
+    smallsPoints[1].innerText = machinePoints;
+
+    // <img class="carta" src="assets/cartas/2C.png" />
+    // add imgLetter
+    const imgLetter = document.createElement("img");
+    imgLetter.src = `assets/cartas/${letter}.png`; // 3H, 2D ...
+    imgLetter.classList.add("carta");
+    divLetterMachine.append(imgLetter);
+
+    if (minPoints > 21) {
+      break;
+    }
+  } while (machinePoints < minPoints && minPoints <= 21);
+
+  setTimeout(() => {
+    if (machinePoints === minPoints) {
+      alert("Nadie gana!");
+    } else if (minPoints > 21) {
+      alert("Computadora gana!");
+    } else if (machinePoints > 21) {
+      alert("Jugador gana!");
+    } else {
+      alert("Computadora gana!");
+    }
+  }, 1000);
 };
 
 // Eventos
@@ -62,7 +101,7 @@ btnAsk.addEventListener("click", () => {
 
   playerPoints = playerPoints + letterValue(letter);
 
-  smalls[0].innerText = playerPoints;
+  smallsPoints[0].innerText = playerPoints;
 
   // <img class="carta" src="assets/cartas/2C.png" />
   // add imgLetter
@@ -74,8 +113,36 @@ btnAsk.addEventListener("click", () => {
   if (playerPoints > 21) {
     console.warn("GAME OVER!");
     btnAsk.disabled = true;
+    btnStop.disabled = true;
+    computerShift(playerPoints);
   } else if (playerPoints === 21) {
-    console.info("GANASTE!");
+    console.info("YOU WIN!");
     btnAsk.disabled = true;
+    btnStop.disabled = true;
+    computerShift(playerPoints);
   }
+});
+
+btnStop.addEventListener("click", () => {
+  btnAsk.disabled = true;
+  btnStop.disabled = true;
+
+  computerShift(playerPoints);
+});
+
+btnNewGame.addEventListener("click", () => {
+  console.clear();
+  deck = [];
+  deck = createDeck();
+  playerPoints = 0;
+  machinePoints = 0;
+
+  smallsPoints[0].innerText = 0;
+  smallsPoints[1].innerText = 0;
+
+  divLetterPlayer.innerHTML = "";
+  divLetterMachine.innerHTML = "";
+
+  btnAsk.disabled = false;
+  btnStop.disabled = false;
 });
