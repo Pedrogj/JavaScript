c71317c5ec8025af05482aa1f83e873501ef5fd1
Pedro Perez <pedrodrums2015@gmail.com>
2022-05-06 10:51:27 -0400

    add set and get

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,14 +1,35 @@
 class Persona {
+  static _conteo = 0;
+  static get conteo() {
+    return Persona._conteo + " instancias";
+  }
+  static mensaje() {
+    console.log("Hola a todos soy un metodo estático");
+  }
+
   nombre = "";
   codigo = "";
   frase = "";
+  comida = "";
 
   constructor(nombre, codigo, frase) {
     this.nombre = nombre;
     this.codigo = codigo;
     this.frase = frase;
+
+    Persona._conteo++;
   }
 
+  // Set y Get
+  set setComidaFavorita(comida) {
+    this.comida = comida;
+  }
+
+  get getComidaFavorita() {
+    return `La comida favorita de ${this.nombre} es ${this.comida}`;
+  }
+
+  // Metodos
   quienSoy() {
     console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
   }
@@ -20,7 +41,15 @@ class Persona {
 
 const spiderMan = new Persona("Peter Parker", "Spider", "Soy spiderman");
 const ironMan = new Persona("Tony Stark", "Iron Man", "Soy Iron Man");
-// console.log(spiderMan, ironMan);
 
 spiderMan.miFrase();
-ironMan.miFrase();
+// ironMan.miFrase();
+
+spiderMan.setComidaFavorita = "Pastel de cereza";
+
+// console.log(spiderMan.getComidaFavorita);
+// console.log(spiderMan);
+// Persona._conteo = 2;
+console.log("Conteo estático", Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
